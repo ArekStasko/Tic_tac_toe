be347eb9a0039b147bd035cb47795f6ad98ef12d
ArekStasko <arek.stasko@gmail.com>
2021-11-09 11:59:31 -0800

    add full game

--- ticTacToe/Program.cs
+++ ticTacToe/Program.cs
@@ -4,17 +4,23 @@ namespace ticTacToe
 {
     class Program
     {
-        static char[,] playField =
+        static char[,] templateField =
         {
            {'1', '2', '3' },
            {'4', '5', '6' },
            {'7', '8', '9' }
         };
 
+        static char[,] playField = new char[3, 3];
+
+
         static void Main(string[] args)
         {
             int player = 2;
 
+            Array.Copy(templateField, 0, playField, 0, templateField.Length);
+
+            int roundsCount = 1;
             do
             {
                 if (player == 2)
@@ -24,13 +30,19 @@ namespace ticTacToe
 
                 checkWinner();
                 SetField(player);
-                
+                if (roundsCount == 9)
+                {
+                    resetPlayField();
+                    Console.WriteLine("No winner !");
+                    roundsCount = 1;
+                }
+                roundsCount++;
+
             } while (true);
         }
 
         public static void SetField(int playerNumber)
         {
-            //Console.Clear();
             Console.WriteLine(" _________________");
             Console.WriteLine("|     |     |     |");
             Console.WriteLine("|  {0}  |  {1}  |  {2}  |", playField[0, 0], playField[0, 1], playField[0, 2]);
@@ -44,6 +56,7 @@ namespace ticTacToe
 
             Console.WriteLine($"Player {playerNumber} turn: ");
             string userSelectionField = Console.ReadLine();
+
             bool correctInput = Int32.TryParse(userSelectionField, out int insertNumber);
 
             while (!correctInput || insertNumber < 1 || insertNumber > 9 || userSelectionField.Length != 1)
@@ -71,24 +84,42 @@ namespace ticTacToe
                 return 'X';
         }
 
-        //TODO: end the algo for winning player 
+        public static void resetPlayField(char winner = 'n')
+        {
+            Console.Clear();
+            Array.Copy(templateField, 0, playField, 0, templateField.Length);
+            if (winner != 'n')
+                Console.WriteLine($"Player {winner} win !");
+        }
+
         public static void checkWinner()
         {
-            char currentSign = playField[0,0];
-            int signsCount = 0;
-            for(int i=0; i<playField.GetLength(0); i++)
+            char[] playerChars = { 'X', 'O' };
+            foreach (char playerChar in playerChars)
             {
-                for(int j=0; j<playField.GetLength(1); j++)
+                for (int i = 0; i < playField.GetLength(0); i++)
                 {
-                    if (currentSign == playField[i, j])
-                        signsCount++;
-                    else signsCount = 0;
 
-                    Console.WriteLine($"Sign: {currentSign}; count of signs: {signsCount}; i: {i}; j: {j}");
+                    if ((playField[i, 0] == playerChar) && (playField[i, 1] == playerChar) && (playField[i, 2] == playerChar))
+                    {
+                        resetPlayField(playerChar);
+                    }
+                    else if ((playField[0, i] == playerChar) && (playField[1, i] == playerChar) && (playField[2, i] == playerChar))
+                    {
+                        resetPlayField(playerChar);
+                    }
+                    else if (
+                        ((playField[0, 0] == playerChar) && (playField[1, 1] == playerChar) && (playField[2, 2] == playerChar))
+                        ||
+                        ((playField[0, 2] == playerChar) && (playField[1, 1] == playerChar) && (playField[2, 0] == playerChar))
+                        )
+                    {
+                        resetPlayField(playerChar);
+                    }
 
-                    currentSign = playField[i, j];
                 }
             }
+
         }
     }
 }
